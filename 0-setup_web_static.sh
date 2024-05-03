7be301c50d5b5d5733e8c7e91fc5a5b25305b331
Abdelillahkadiri <abdelillah2954@gmail.com>
2024-05-03 03:34:02 -0700

    corr

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-
-# Install Nginx if not already installed
 if ! dpkg -s nginx > /dev/null 2>&1; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 fi
-
-# Create necessary directories if they don't exist
 sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo touch /data/web_static/releases/test/index.html
-echo "Hello, this is a test index page." | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create symbolic link /data/web_static/current
+echo "Hello, this is a test index page.
+"|sudo tee /data/web_static/releases/test/index.html>/dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/^\s*location \/static\/ {\s*$/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$config_file"
-
-# Restart Nginx
+sudo sed -i '/^\s*location \/static\/ {\s*$/a \\tlocation /hbnb_static/
+{\n\t\talias /data/web_static/current/;\n\t}\n'
+"$config_file"
 sudo service nginx restart
